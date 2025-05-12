5d42d7a2bab82a19fe99f8d68ebac3fc3d4a8b3e
tuhin2701 <tuhin.td.06.27@gmail.com>
2025-05-12 13:28:39 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+
+a=input('Enter the first number: ')
+a=int(a)
+b=input('Enter the second number: ')
+b=int(b)
+
+print('Addition: ', a+b)
+print('Substraction: ', a-b)
+print('Multiplication: ', a*b)
+print('Division: ', a/b)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+a=input('Enter your first name: ')
+b=input('Enter your second name: ')
+
+print('Helo, '+a+' '+b+'!','Welcome to te Python program')
